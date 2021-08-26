982190c1795dcf971f8c24b29a656d1aa5e63187
Sigfrid Dusci <sigfrid@users.noreply.github.com>
2021-08-26 10:09:57 +0200

    Add SAML authentication

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ ruby '2.7.4'
 
 gem 'sinatra', '~> 2.1'
 gem 'puma', '~> 5.4'
+
+gem 'dotenv', '~> 2.7', '>= 2.7.6'
+gem 'ruby-saml', '~> 1.12', '>= 1.12.2'
--- app.rb
+++ app.rb
@@ -1,6 +1,66 @@
 require 'bundler'
 Bundler.require
 
+def saml_settings
+  settings = OneLogin::RubySaml::Settings.new
+
+  url_base ||= "https://localhost:3000"
+
+  # When disabled, saml validation errors will raise an exception.
+  # settings.soft = true
+
+  #SP section
+  settings.sp_entity_id                   = url_base + "/saml/metadata"
+  settings.assertion_consumer_service_url = url_base + "/saml/acs"
+  settings.assertion_consumer_logout_service_url = url_base + "/saml/logout"
+
+  # IdP section
+  settings.idp_entity_id                  = "https://sts.windows.net/#{ENV['MICROSOFT_APP_ID']}/"
+  settings.idp_sso_target_url             = "https://login.microsoftonline.com/#{ENV['MICROSOFT_APP_ID']}/saml2"
+  settings.idp_slo_target_url             = "https://login.microsoftonline.com/#{ENV['MICROSOFT_APP_ID']}/saml2"
+  ####settings.idp_cert                       = ""
+  settings.idp_cert_fingerprint           = "361F0706F00F9558CA9D71E5110B1A71A36DBCA8"
+  settings.idp_cert_fingerprint_algorithm =  XMLSecurity::Document::SHA1 #"http://www.w3.org/2000/09/xmldsig#sha1"
+
+
+  settings.name_identifier_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+
+  # Security section
+  settings.security[:authn_requests_signed] = false
+  settings.security[:logout_requests_signed] = false
+  settings.security[:logout_responses_signed] = false
+  settings.security[:metadata_signed] = false
+  settings.security[:digest_method] = XMLSecurity::Document::SHA1
+  settings.security[:signature_method] = XMLSecurity::Document::RSA_SHA1
+
+  return settings
+end
+
+post '/saml/metadata' do
+  content_type 'text/xml'
+  OneLogin::RubySaml::Metadata.new.generate(saml_settings)
+end
+
+post '/saml/acs' do
+  response = OneLogin::RubySaml::Response.new(params[:SAMLResponse])
+  response.settings = saml_settings
+
+  # We validate the SAML Response and check if the user already exists in the system
+  if response.is_valid?
+    # authorize_success, log the user
+    p "VALID !!!!!"
+    @nameid = response.nameid
+    # p response.attributes
+    erb :in
+  else
+    p " FAILURE"
+    @errors = response.errors
+    erb :out
+      # List of errors is available in response.errors array
+  end
+end
+
 get '/' do
-  erb :index
+  request = OneLogin::RubySaml::Authrequest.new
+  redirect request.create(saml_settings)
 end
